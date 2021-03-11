2b3ce064c0bd45f0b0b8ef44c244640c42c7ec62
Alexander Campo <jalexcam@gmail.com>
2021-03-11 00:25:50 -0500

    Added content of middleware

--- middleware.php
+++ middleware.php
@@ -1,1 +1,10 @@
 # Middleware
+
+- [Introduction](#introduction)
+- [Defining Middleware](#defining-middleware)
+- [Registering Middleware](#registering-middleware)
+    - [Global Middleware](#global-middleware)
+    - [Assigning Middleware To Routes](#assigning-middleware-routes)
+    - [Middleware Groups](#middleware-groups)
+
+- [Middleware Parameters](#middleware-parameters)
